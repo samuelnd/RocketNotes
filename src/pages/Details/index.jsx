2139bef1020e789b53693c769f215ce8ccf0d80f
samuel <samuelsnds09@outlook.com>
2022-07-17 14:49:11 -0300

    application initialized

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles"
+
+export function Details() {
+  return(
+    <>
+      <Container> 
+        <h1>Hello World</h1>
+
+        <p>Criando texto com reactjs</p>
+      </Container>
+    </>
+  )
+}
