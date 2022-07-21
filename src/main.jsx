0d923ed4231027011951f2902ffa43b241b2eafb
samuel <samuelsnds09@outlook.com>
2022-07-21 10:10:04 -0300

    created page NewNote and page Profile

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import {ThemeProvider} from 'styled-components';
 import GlobalStyles from './styles/global';
 
 import theme from './styles/theme';
-import {Home} from './pages/Home';
+import {Profile} from './pages/Profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home/>
+      <Profile/>
     </ThemeProvider>
   </React.StrictMode>
 )
+ 
