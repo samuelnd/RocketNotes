735780ce1e5595af724cdb361624cdb99f63b5c3
samuel <samuelsnds09@outlook.com>
2022-08-12 18:26:33 -0300

    update hooks

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 import { Background, Container, Form } from "./styles";
 import { FiMail, FiLock} from "react-icons/fi";
 import {Link} from 'react-router-dom';
+
+import {useAuth} from "../../hooks/auth";
 
 import {Input} from "../../components/Input";
 import {Button} from "../../components/Button";
 
 
 export function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const {signIn} = useAuth();
+
+    function handleSignIn() {
+        signIn({email, password});
+    }
+
     return (
         <Container>
             <Form>
@@ -19,14 +33,17 @@ export function SignIn() {
                     placeholder="email"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
                 <Input 
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
+
                 />
 
-                <Button title="Entrar" />
+                <Button title="Entrar" onClick={handleSignIn}/>
                 
                 <Link to="/register">Criar Conta </Link>
 
